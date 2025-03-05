62726da63b44bbf01574d6ca9faa5bf262b5b688
Shashank171204 <shashu1712@gmail.com>
2025-03-05 21:38:52 +0530

    update

---
+++ griet/react-store-b/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div align="center" class="row">
+        <h2>Login Form</h2>
+        <p><input type="text" name="user" id="user" placeholder="Enter Usename"></input></p>
+        <p><input type="password" name="pwd" id="pwd" placeholder="Enter Password"></input></p>
+        <p><button>Log In</button></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
